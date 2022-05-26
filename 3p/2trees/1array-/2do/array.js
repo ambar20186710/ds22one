e55c01b3acd72169b8d42368ed6dec3853519a6a
Ambar  Aguilar <98919944+ambar20186710@users.noreply.github.com>
2022-05-26 11:55:58 -0500

    Update array.js

--- 3p/2trees/1array-/2do/array.js
+++ 3p/2trees/1array-/2do/array.js
@@ -1,17 +1,62 @@
-  function Node(o){
-      this.d=o//bject<->data
-      this.l=null//eft
-      this.r=null//ight
+  class Node {
+  constructor(data) {
+    this.data = data;
+    this.hijo = [];
   }
+}
+
+class Arbol {
+  constructor() {
+    this.root = null;
+  }
+
+  add(data, toNodeData) {
+    const node = new Node(data);
+    const parent = toNodeData ? this.findBFS(toNodeData) : null;
+
+    if (parent) {
+      parent.hijo.push(node);
+    } else {
+      if (!this.root) {
+        this.root = node;
+      } else {
+        return "error 1";
+      }
+    }
+  }
+
+  findBFS(data) {
+    let _node = null;
+
+    this.traverseBFS((node) => {
+      if (node.data == data) {
+        _node = node;
+      }
+    });
+    return _node;
+  }
+
+  traverseBFS(cb) {
+    const queue = [this.root];
+
+    if (cb) {
+      while (queue.length) {
+        const node = queue.shift();
+
+        cb(node);
+
+        for (const child of node.hijo) {
+          queue.push(child);
+        }
+      }
+    }
+  }
+}
   
-  let r = new Node("A")
-  let o1 = new Node( "B")
-  let o2 = new Node ("C")
-  let arr = []
-  
-  r.l = o1
-  r.r = o2
-  
-  arr.push(r)
-  
-  console.log(arr)
+
+let arbol = new Arbol();
+arbol.add("1");
+arbol.add("2", "1");
+arbol.add("4", "2");
+arbol.add("1", "2");
+arbol.traverseBFS((node) => console.log(node));
