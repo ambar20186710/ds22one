824445907f1d94707beb728c4b5f2a1a6f1398c7
Ambar  Aguilar <98919944+ambar20186710@users.noreply.github.com>
2022-05-31 13:18:27 -0500

    Update matrix.js

--- 3p/3graphs/array-/2do/matrix.js
+++ 3p/3graphs/array-/2do/matrix.js
@@ -15,9 +15,7 @@
         let evaluar; 
         evaluar = this.matriz[i].length;
         
-        if(evaluar == this.matriz.length){
-        }
-        else{
+        if(evaluar != this.matriz.length){
           return console.log("No es cuadrada");
         }
       }
@@ -27,17 +25,35 @@
     isSymmetrical(){
       for(let i=0; i < this.matriz.length; i++){
         for(let j=0; j < this.matriz[i].length; j++){
-         if(this.matriz[i][j] == this.matriz[j][i]){
+         if(this.matriz[i][j] != this.matriz[j][i]){
+           return console.log("No es simetrica");
          }
-          else{
-            return console.log("No es simetrica");
-        }
        } 
    	 }
       return console.log("Es simetrica");
     }
     
     isTree(){
+      let sum=0;
+      let contador=0;
+      let columnas=0;
+      
+       for(let i=0; i < this.matriz.length; i++){
+        for(let j=0; j < this.matriz[i].length; j++){
+          contador++;
+          columnas = contador / this.matriz.length ;
+        }
+       }
+          
+      for(let i=1;i<this.matriz.length;i++){
+      	for(let j=0;j<columnas;j++){
+        	if(j<i){
+            
+       			
+           }
+       	}
+     	}
+                
     }
     
     isGraph(){
